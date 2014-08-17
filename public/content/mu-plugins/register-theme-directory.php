71f4268457a89c6ed16252fc926215d4ce5209d1
Pirate Dunbar <yarr@piratedunbar.com>
2014-08-17 04:14:34 -0400

    Update register-theme-directory.php
    removed extra trailing slash that gets added due to

--- public/content/mu-plugins/register-theme-directory.php
+++ public/content/mu-plugins/register-theme-directory.php
@@ -2,4 +2,4 @@
 /**
  * Register the default WordPress themes directory.
  */
-register_theme_directory( ABSPATH . 'wp-content/themes/' );
+register_theme_directory( ABSPATH . 'wp-content/themes' );
