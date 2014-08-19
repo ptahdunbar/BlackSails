ac6c013bbccb44020c613c60826182a3a750e3d9
Pirate Dunbar <yarr@piratedunbar.com>
2014-08-18 20:41:09 -0400

    Update loader.php

--- wp-cli/loader.php
+++ wp-cli/loader.php
@@ -2,7 +2,13 @@
 /**
  * Loads custom wp-cli commands from this directory.
  */
+ 
+ // Setup WP_ENV so the app knows that this is coming from a cli request.
+if ( ! defined('WP_ENV') ) {
+	define('WP_ENV', 'wp-cli');
+}
 
+// Exclude files from inside the wp-cli/ dir that you don't want included by wp-cli.
 $skip = [ 'loader.php' ];
 
 foreach ( glob(dirname(__FILE__) . '/*.php') as $wpcli_command ) {
