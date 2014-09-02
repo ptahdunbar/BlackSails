ba76c98ffe7f61300c62f5d9e8723406520f8eb8
Pirate Dunbar <yarr@piratedunbar.com>
2014-09-02 10:32:43 -0400

    Update global-config.php

--- config/global-config.php
+++ config/global-config.php
@@ -11,14 +11,37 @@
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+if ( ! defined('AUTH_KEY') ) {
+	define('AUTH_KEY', 'put your unique phrase here');
+}
+
+if ( ! defined('SECURE_AUTH_KEY') ) {
+	define('SECURE_AUTH_KEY', 'put your unique phrase here');
+}
+
+if ( ! defined('LOGGED_IN_KEY') ) {
+	define('LOGGED_IN_KEY', 'put your unique phrase here');
+}
+
+if ( ! defined('NONCE_KEY') ) {
+	define('NONCE_KEY', 'put your unique phrase here');
+}
+
+if ( ! defined('AUTH_SALT') ) {
+	define('AUTH_SALT', 'put your unique phrase here');
+}
+
+if ( ! defined('SECURE_AUTH_SALT') ) {
+	define('SECURE_AUTH_SALT', 'put your unique phrase here');
+}
+
+if ( ! defined('LOGGED_IN_SALT') ) {
+	define('LOGGED_IN_SALT', 'put your unique phrase here');
+}
+
+if ( ! defined('NONCE_SALT') ) {
+	define('NONCE_SALT', 'put your unique phrase here');
+}
 
 /**#@-*/
 
