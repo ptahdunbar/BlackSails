feb2afaf1e823446bbd5dd184187ee6924d9aaf9
Pirate Dunbar <ptah@piratedunbar.com>
2014-12-13 15:52:42 -0500

    Update sample-config.php

--- config/sample-config.php
+++ config/sample-config.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * The tests configuration file for WordPress.
- */
 
 /** The name of the database for WordPress */
 define('DB_NAME', 'database_name_here');
@@ -15,31 +12,15 @@ define('DB_PASSWORD', 'password_here');
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
-/**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
-define('WPLANG', '');
-
 /**#@-*/
 
 /** PHP error reporting. */
-define('WP_DEBUG', true);
-define('WP_DEBUG_DISPLAY', true);
-define('WP_DEBUG_LOG', false); // lies. storing logs in logs/ dir.
+// define('WP_DEBUG', true);
+// define('WP_DEBUG_DISPLAY', WP_DEBUG);
+// define('WP_DEBUG_LOG', false); // lies. storing logs in logs/ dir.
 
 /** Disable minified scripts and styles and use full versions instead. */
-define('SCRIPT_DEBUG', true);
+// define('SCRIPT_DEBUG', WP_DEBUG);
 
 /** Saves database queries for query analyzing. Not recommended for production. */
-define('SAVEQUERIES', true);
+// define('SAVEQUERIES', WP_DEBUG);
