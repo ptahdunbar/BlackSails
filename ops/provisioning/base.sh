0aed8721490a58f92ee1a3bab6337bc93da27a86
Ptah Dunbar <yarr@piratedunbar.com>
2015-03-01 06:50:12 -0500

    Save time on recurring apt-get updates.

--- ops/provisioning/base.sh
+++ ops/provisioning/base.sh
@@ -3,7 +3,11 @@
 echo "DevOps provisioning: base"
 date > /etc/vagrant_provisioned_at
 
-sudo apt-get update
+if [ ! -e "~/.provisioned_at" ]; then
+	sudo apt-get update
+	#sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoremove
+	touch .provisioned_at
+fi
 
 # Set the Server Timezone to CST
 echo "America/New_York" > /etc/timezone
@@ -16,6 +20,11 @@ sudo apt-get -y install git-core subversion
 sudo apt-get -y install ntp
 sudo apt-get -y install cachefilesd
 sudo echo "RUN=yes" > /etc/default/cachefilesd
+
+# see http://docs.moodle.org/dev/Table_of_locales
+LOCALE_LANGUAGE="en_US"
+LOCALE_CODESET="en_US.UTF-8"
+sudo locale-gen $LOCALE_LANGUAGE $LOCALE_CODESET
 
 echo "-+-+-+- software versions:"
 git --version
