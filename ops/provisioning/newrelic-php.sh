5d58853d4f04e01fcfadd2f4e5bf3b926d56e226
Pirate Dunbar <ptah@piratedunbar.com>
2015-05-27 15:18:19 -0400

    Delete newrelic-php.sh

--- ops/provisioning/newrelic-php.sh
+++
@@ -1,9 +0,0 @@
-wget -O - https://download.newrelic.com/548C16BF.gpg | sudo apt-key add -
-
-sudo sh -c 'echo "deb http://apt.newrelic.com/debian/ newrelic non-free" > /etc/apt/sources.list.d/newrelic.list'
-
-sudo apt-get update
-sudo apt-get install newrelic-php5
-sudo newrelic-install install
-
-echo newrelic.license="6afb302ecf8f306d1d78190da805807e1b28ed7e" > /etc/php5/apache2/conf.d/newrelic.conf
